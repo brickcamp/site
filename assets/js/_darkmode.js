e5fe2f0e30a811dea5c6a071ef409ae087b3ee07
Tobias Buckdahn <info@brickup.de>
2025-07-05 17:43:25 +0200

    Tinkering with site generation

---
+++ assets/js/_darkmode.js
@@ -0,0 +1,34 @@
+let isDark = false;
+
+function init() {
+  switch (localStorage.getItem("theme")) {
+    case "dark":
+      setDark(true);
+      return;
+    case "light":
+      setDark(false);
+      return;
+  }
+
+  const darkMatcher = window.matchMedia("(prefers-color-scheme: dark)");
+  setDark(darkMatcher.matches);
+}
+
+function setDark(enabled) {
+  isDark = enabled;
+
+  const theme = isDark ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+}
+
+function toggle() {
+  setDark(!isDark);
+}
+
+function initToggle() {
+  document.getElementById("darkmode-toggle").addEventListener("click", toggle);
+}
+
+init();
+window.addEventListener("DOMContentLoaded", initToggle);
---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
+
+const purgecss = purgeCSSPlugin({
+  content: ["./hugo_stats.json"],
+  defaultExtractor: (content) => {
+    const els = JSON.parse(content).htmlElements;
+    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+  },
+  safelist: {
+    deep: [/dropdown-menu$/],
+    greedy: [/data-bs-theme/],
+  },
+});
+
+export default {
+  plugins: [
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+  ],
+};
