8154ba51998eb8712272d429f3e81da924b94511
Tobias Buckdahn <info@brickup.de>
2025-12-07 20:32:02 +0100

    Filter entries via dropdown links

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,51 @@
 const params = new URLSearchParams(window.location.search);
-var base = params.get("base") || "__any";
-var type = params.get("type") || "__any";
+var base  = params.get("base")  || "__any";
+var type  = params.get("type")  || "__any";
 var value = params.get("value") || "__any";
-var sort = params.get("sort") || "date-desc";
+var sort  = params.get("sort")  || "date-desc";
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadEntries();
+  initFilterNav();
+  refresh();
 });
+
+function initFilterNav() {
+  const links = document.querySelectorAll("#filter-nav .dropdown-item");
+  links.forEach(link => link.addEventListener("click", onFilterLinkClicked));
+}
+
+function refresh() {
+  refreshFilterDropdowns();
+  refreshEntries();
+}
+
+function refreshFilterDropdowns() {
+  const dropdowns = document.querySelectorAll("#filter-nav .nav-item.dropdown");
+  const selected = {
+    "nav-base": base,
+    "nav-type": type,
+    "nav-value": value,
+    "nav-sort": sort,
+  }
+  dropdowns.forEach(d => copySpanText(d.id + "-" + selected[d.id], d.id));
+}
+
+async function refreshEntries() {
+  const csvSorted = "/sorted/" + sort;
+  const csvFilter = "/filtered/" + base + "-" + type + "-" + value;
+
+  clearEntries();
+  Promise.all([loadCSV(csvSorted), loadCSV(csvFilter)])
+    .then(([listSort, listFilter]) => {
+      const indexMap = new Map(listSort.map((item, index) => [item, index]));
+      listFilter
+        .map(line => line.split(","))
+        .filter((entry) => indexMap.has(entry[0]))
+        .sort((a, b) => indexMap.get(a[0]) - indexMap.get(b[0]))
+        .forEach(appendEntry);
+    })
+    .catch((err) => console.error("refreshEntries failed:", err));
+}
 
 async function loadCSV(url) {
   const response = await fetch("/data/" + url + "/index.csv");
@@ -18,28 +57,11 @@ async function loadCSV(url) {
     .filter((line) => line.length > 0);
 }
 
-async function loadEntries() {
-  const csvSorted = "/sorted/" + sort;
-  const csvFilter = "/filtered/" + base + "-" + type + "-" + value;
-
-  clearEntries();
-  Promise.all([loadCSV(csvSorted), loadCSV(csvFilter)])
-    .then(([listSort, listFilter]) => {
-      const indexMap = new Map(listSort.map((item, index) => [item, index]));
-      entries = listFilter
-        .map(line => line.split(","))
-        .filter((entry) => indexMap.has(entry[0]))
-        .sort((a, b) => indexMap.get(a[0]) - indexMap.get(b[0]));
-      entries.forEach(addEntry);
-    })
-    .catch((err) => console.error("loadEntries failed:", err));
-}
-
 function clearEntries() {
   document.getElementById("entries").textContent = "";
 }
 
-function addEntry(entry) {
+function appendEntry(entry) {
   const elCol = document.createElement("div");
   elCol.classList.add("col");
   document.getElementById("entries").appendChild(elCol);
@@ -62,4 +84,46 @@ function addEntry(entry) {
   elLink.href = entry[0];
   elLink.classList.add("stretched-link");
   elCard.appendChild(elLink);
+}
+
+function onFilterLinkClicked(e) {
+  const id = e.target.closest("[id]")?.id;
+  const segments = id.split("-", 3);
+  if (segments.length < 2 || segments[0] != "nav") {
+    return;
+  }
+
+  switch (segments[1]) {
+    case "base":
+      base = segments[2];
+      break;
+    case "type":
+      type = segments[2];
+      break;
+    case "value":
+      value = segments[2];
+      break;
+    case "sort":
+      sort = segments[2];
+      break;
+    default:
+      return;
+  }
+  refresh();
+}
+
+function copySpanText(from, to) {
+  const text = getSpanText(from);
+  setSpanText(to, text);
+}
+
+function getSpanText(id) {
+  return document.getElementById(id)?.querySelector("span")?.innerText;
+}
+
+function setSpanText(id, text) {
+  const span = document.getElementById(id)?.querySelector("span");
+  if (span){
+    span.innerText = text;
+  }
 }
