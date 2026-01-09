e2ee94ed86b2d7b93a66a351a1d67e8f24f17bbe
Tobias Buckdahn <info@brickup.de>
2026-01-09 23:10:50 +0100

    Add unit converter functionality

--- assets/js/unit-converter.js
+++ assets/js/unit-converter.js
@@ -1,19 +1,28 @@
+const elNumber  = document.getElementById("conversion-number");
+const elUnit    = document.getElementById("conversion-unit");
+const elUnits   = document.querySelectorAll("#conversion-units [data-factor]");
+const elScale   = document.getElementById("conversion-scale");
+const elScales  = document.querySelectorAll("#conversion-scales [data-factor]");
+const elResults = document.querySelectorAll("#conversion-results input");
+
 document.addEventListener("DOMContentLoaded", () => {
-  initDropdowns();
-  refresh();
+  initInputs();
+  onNumberInputChanged();
 });
 
-function initDropdowns() {
-  const unit = document.querySelectorAll("#conversion-unit-picker .dropdown-item");
-  unit.forEach(link => {
+function initInputs() {
+  elNumber.addEventListener("input", onNumberInputChanged);
+  elUnits.forEach(link => {
+    link.classList.toggle("active", link.dataset.factor == elUnit.dataset.factor);
     link.addEventListener("click", onUnitLinkClicked);
     link.addEventListener("keydown", e => {
       if (e.key === "Enter") onUnitLinkClicked(e);
     });
   });
 
-  const scale = document.querySelectorAll("#conversion-scale-picker .dropdown-item");
-  scale.forEach(link => {
+  elScale.addEventListener("input", onScaleInputChanged);
+  elScales.forEach(link => {
+    link.classList.toggle("active", link.dataset.factor == elScale.dataset.factor);
     link.addEventListener("click", onScaleLinkClicked);
     link.addEventListener("keydown", e => {
       if (e.key === "Enter") onScaleLinkClicked(e);
@@ -22,15 +31,81 @@ function initDropdowns() {
 }
 
 function onUnitLinkClicked(e) {
-  const link = e.target.closest("[data-unit]");
-  console.log(link?.dataset.unit);
+  const selected = e.target.closest("[data-factor]");
+  elUnit.dataset.factor = selected.dataset.factor;
+  elUnit.innerText      = selected.innerText;
+
+  elUnits.forEach((el) => el.classList.remove("active"));
+  selected.classList.add("active");
+
+  calculate();
 }
 
 function onScaleLinkClicked(e) {
-  const link = e.target.closest(".dropdown-item");
-  console.log(link?.innerText);
+  const selected = e.target.closest("[data-factor]");
+  elScale.dataset.factor = selected.dataset.factor;
+  elScale.value          = selected.dataset.factor == "1" ? "× 1" : selected.innerText;
+
+  elScales.forEach((el) => el.classList.remove("active"));
+  selected.classList.add("active");
+
+  calculate();
 }
 
-function refresh() {
+function onNumberInputChanged() {
+  elNumber.dataset.factor = elNumber.valueAsNumber;
 
+  calculate();
+}
+
+function onScaleInputChanged() {
+  const MULTIPLIERS = /[·×xX*]/g;
+  const DIVIDERS    = /[\\\/∕⁄÷:]/g;
+  const PERCENTAGE  = /^[\d.,]+%$/g;
+  const RATIO       = /^[\d.,]+:[\d.,]+$/g;
+  
+  var input = elScale.value
+      .replace(MULTIPLIERS, "x")
+      .replace(DIVIDERS, ":")
+      .replace(/[^\d.,:x%]/g, "");
+
+  var factor;
+  if (input.match(PERCENTAGE)) {
+    factor = Number(input.replace("%", "")) / 100;
+  } else if (input.match(RATIO)) {
+    const ratio = input.split(":", 2);
+    factor = Number(ratio[0]) * 1.0 / Number(ratio[1]);
+  } else if (input.match(DIVIDERS)) {
+    factor = 1.0 / Number(input.replace(/[^\d.,]/g, ""));
+  } else {
+    factor = Number(input.replace(/[^\d.,]/g, ""));
+  }
+
+  elScale.dataset.factor = isNaN(factor) ? "" : factor;
+  elScales.forEach((el) => el.classList.remove("active"));
+  calculate();
+}
+
+function setActive(element, active) {
+  active ? element.classList.add("active") : element.classList.remove("active");
+}
+
+function calculate() {
+  const number = Number(elNumber.dataset.factor);
+  const unit   = Number(elUnit.dataset.factor);
+  const scale  = Number(elScale.dataset.factor);
+  const input  = number * unit * scale;
+
+  if (isNaN(input)) {
+    elResults.forEach((result) => result.value = "");
+    return;
+  }
+
+  elResults.forEach((result) => {
+    const conversion = Number(result.dataset.factor);
+    result.value = Number((input / conversion).toFixed(3));
+    if (result.value == 0) {
+      result.value = "≈ 0";
+    }
+  });
 }
