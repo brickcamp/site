43a7c9b728c514811679022dc33b328984e9be5d
Tobias Buckdahn <info@brickup.de>
2026-03-02 00:28:32 +0100

    Fix size tooltip for touch devices

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,3 +8,8 @@ btnToTop.addEventListener("click", () => {
 window.addEventListener("scroll", () => {
   btnToTop.style.display = window.scrollY > 200 ? "block" : "none";
 });
+
+const tooltipTrigger = (window.matchMedia('(pointer: fine)').matches) ? 'hover focus' : 'click';
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(el => {
+  new bootstrap.Tooltip(el, { trigger: tooltipTrigger });
+})
